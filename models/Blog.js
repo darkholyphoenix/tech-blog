a8ab0bdfa81b91edfd0dc226d68bf07a81082c4d
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-08-13 20:48:31 -0500

    added controllers and models

---
+++ models/Blog.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our blog model
+class Blog extends Model {} 
+      
+// create fields/columns for blog model
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    blog_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog'
+  }
+);
+
+module.exports = Blog;
